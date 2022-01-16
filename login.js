6cdf8d5c829bd5ca6d6e9cda8d6b275b6f21dde2
rohit1125 <66976592+rohit1125@users.noreply.github.com>
2022-01-17 01:32:58 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="rohit" && password=="rohit123")
+    {
+        
+        
+        window.location = "welcome.html";
+        
+        
+    }
+    else
+    {
+        alert ("login failed");
+        r
+    }
+}
